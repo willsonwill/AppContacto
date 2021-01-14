49faf9d3856158a98f48aab6f7a8bca3a9a3ddc8
wilson chojllu coa <willsonch.will@gmail.com>
2021-01-13 20:36:16 -0400

    Proyecto Inicial

---
+++ resources/views/contacto/registro.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts/contentLayoutMaster')
+@section('title', 'App Calender')
+@section('vendor-style')
+@endsection
+@section('page-style')
+@endsection
+@section('content')
+
+
+
+
+
+@endsection
+@section('vendor-script')
+@endsection
+@section('page-script')
+@endsection
+
---
+++ resources/views/vendedor/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts/contentLayoutMaster')
+
+@section('vendor-style')
+
+@endsection
+@section('page-style')
+
+@endsection
+@section('content')
+
+<section id="basic-examples">
+  <div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-content">
+          <div class="card-body">
+                <h1>Listado Vendedores</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+</section>
+
+@endsection
+
+@section('vendor-script')
+
+@endsection
+@section('page-script')
+      
+@endsection
+
